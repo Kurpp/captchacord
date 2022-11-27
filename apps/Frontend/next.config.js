71b533dd69a878d3a4f50fea35f05a5909a20201
Kurp <78627265+Kurpp@users.noreply.github.com>
2022-11-26 20:46:49 -0500

    feat: obfuscate class names

--- apps/Frontend/next.config.js
+++ apps/Frontend/next.config.js
@@ -1,7 +1,26 @@
+const MangleCssClassPlugin = require("mangle-css-class-webpack-plugin");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
+  },
+  webpack: (config, { dev }) => {
+    if (!dev) {
+      config.plugins.push(
+        new MangleCssClassPlugin({
+          classNameRegExp:
+            "((hover|focus|xs|md|sm|lg|xl)[\\\\]*:)*tw-[a-z_-][a-zA-Z0-9_-]*",
+          ignorePrefixRegExp: "((hover|focus|xs|md|sm|lg|xl)[\\\\]*:)*",
+          log: true,
+          classGenerator: (original) => {
+            return btoa(original).replace(/=/g, "").slice(-7);
+          },
+        })
+      );
+    }
+
+    return config;
   },
 };
 
