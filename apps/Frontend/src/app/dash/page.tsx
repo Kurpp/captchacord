5c827f1d29ff251ba7632456695e3abe7a5867c6
Kurp <78627265+Kurpp@users.noreply.github.com>
2022-12-03 10:37:15 -0500

    refactor(frontend): display mutual guilds separately

--- apps/Frontend/src/app/dash/page.tsx
+++ apps/Frontend/src/app/dash/page.tsx
@@ -7,6 +7,7 @@ import { GuildsResponse } from "../../typings/API";
 
 export default function Home() {
   const router = useRouter();
+
   const { data, error } = useSWR<GuildsResponse>("/discord/guilds", (url) =>
     fetch(`${process.env.API_URL}${url}`, { credentials: "include" }).then(
       (r) => {
@@ -30,26 +31,15 @@ export default function Home() {
           Welcome John Doe
         </h1>
         <div className="tw-grid tw-gap-2 tw-grid-cols-4">
-          {/* TODO: tidy this up */}
-          {data.userGuilds
-            // shortest -> longest
+          {data.botGuilds
             .sort((a, b) => a.name.length - b.name.length)
-            // guilds which have manage server perms first
-            .sort(
-              (a, b) =>
-                Number(
-                  (BigInt(b.permissions!) & BigInt(1 << 5)) === BigInt(1 << 5)
-                ) -
-                Number(
-                  (BigInt(a.permissions!) & BigInt(1 << 5)) === BigInt(1 << 5)
-                )
-            )
-            // guilds that the bot are in first
-            .sort(
-              (a, b) =>
-                Number(data.botGuilds.map(({ id }) => id).includes(b.id)) -
-                Number(data.botGuilds.map(({ id }) => id).includes(a.id))
-            )
+            .map((guild) => (
+              <p key={guild.id}>{guild.name}</p>
+            ))}
+        </div>
+        <div className="tw-grid tw-gap-2 tw-grid-cols-4">
+          {data.userGuilds
+            .sort((a, b) => a.name.length - b.name.length)
             .map((guild) => (
               <p key={guild.id}>{guild.name}</p>
             ))}
