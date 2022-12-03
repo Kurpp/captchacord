6a6a993c1e3efdf9714ab02185de50083470bf12
Kurp <78627265+Kurpp@users.noreply.github.com>
2022-12-03 00:48:15 -0500

    feat(backend): fetch and parse both user and bot guilds

--- apps/Backend/src/routes/Discord.ts
+++ apps/Backend/src/routes/Discord.ts
@@ -1,8 +1,11 @@
 import type { FastifyInstance } from "fastify";
 import fastifyPassport from "@fastify/passport";
+import { Routes, APIGuild } from "discord-api-types/v10";
 import { requireAuth } from "../middlewares/auth_middleware.js";
 
 export default async function (router: FastifyInstance) {
+  const { rest } = router;
+
   router.get(
     "/login",
     fastifyPassport.authenticate("discord", {
@@ -12,12 +15,31 @@ export default async function (router: FastifyInstance) {
   );
 
   router.get(
-    "/",
+    "/guilds",
     {
       preHandler: requireAuth,
     },
     async (req, res) => {
-      res.send(req.user);
+      try {
+        const rawBotGuilds = (await rest.get(
+          Routes.userGuilds()
+        )) as APIGuild[];
+
+        const userGuilds = req.user!.guilds;
+        const userGuildIds = userGuilds?.map((guild) => guild.id);
+        const botGuilds = rawBotGuilds.filter(({ id }) =>
+          userGuildIds?.includes(id)
+        );
+
+        return res.send({
+          botGuilds,
+          userGuilds,
+        });
+      } catch (err) {
+        return res
+          .status(400)
+          .send({ statusCode: 400, message: "Something went wrong" });
+      }
     }
   );
 }
